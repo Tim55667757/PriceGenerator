efab3999d8ce12f63d96a132cccf19cf8ea49b9c
Timur Gilmullin <tim55667757@gmail.com>
2022-07-14 23:55:59 +0300

    Apache-2.0 license added to project files

--- pricegenerator/UniLogger.py
+++ pricegenerator/UniLogger.py
@@ -2,7 +2,24 @@
 #
 # Author: Timur Gilmullin
 
-# This module initialize standard python logging system.
+"""
+This module init standard python logging system with some of pre-defined parameters,
+e.g. debug level for log-file, info level for console, log-rotate and so on.
+"""
+
+# Copyright (c) 2022 Gilmillin Timur Mansurovich
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 
 import sys
