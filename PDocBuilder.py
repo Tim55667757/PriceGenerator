85759d2602b6953470747e815bcf2ec155899c26
Timur Gilmullin <tim55667757@gmail.com>
2023-01-11 20:11:22 +0300

    original code removed from api-doc

--- PDocBuilder.py
+++ PDocBuilder.py
@@ -24,6 +24,13 @@ sys.path.extend([
     os.path.abspath(os.path.join(curdir, "pricegenerator")),
 ])
 
+pdoc.render.configure(
+    docformat="restructuredtext",
+    favicon="https://github.com/Tim55667757/PriceGenerator/blob/develop/media/logo-pricegenerator-256x256px.png?raw=true",
+    footer_text="⚙ Good luck for you in trade automation! And profit!",
+    logo="https://github.com/Tim55667757/PriceGenerator/blob/develop/media/logo-pricegenerator-420x610px.png?raw=true",
+    show_source=False,
+)
 pdoc.pdoc(
     Path("pricegenerator").resolve(),
     output_directory=Path("docs").resolve(),
